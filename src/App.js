0602a26747000d2ddaae5f9e10958c60b332ecdb
milen-ale <milenalebel2@gmail.com>
2022-08-15 09:11:26 -0700

    fix linter error

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit 
-          <code>src/App.js</code> 
+          Edit
+          <code>src/App.js</code>
           and save to reload.
-        </p>,
+        </p>
+        ,
         <a
           className="App-link"
           href="https://reactjs.org"
