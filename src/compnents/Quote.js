a8c3e4691b40502eb0383e14a6b48ab45f768685
milen-ale <milenalebel2@gmail.com>
2022-08-26 06:48:17 -0700

    add styles

--- src/compnents/Quote.js
+++ src/compnents/Quote.js
@@ -4,9 +4,9 @@ function Quote() {
   return (
     <div className="">
       <h1> Math Magiciancian</h1>
-      <p>
-        Matmatics is not about numbers ,equations computation, or algorithm
-        its about understanding. - William paul Thurston
+      <p className="quote">
+        &quot; Matmatics is not about numbers ,equations computation, or algorithm
+        its about understanding. - William paul Thurston&quot;
       </p>
     </div>
 
